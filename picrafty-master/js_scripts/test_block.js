e8d235505b13bb05de05f1491abc3fe23c41ca1f
asunathanr <nrobert2@my.athens.edu>
2019-06-07 13:44:18 -0500

    Initial commit

---
+++ picrafty-master/js_scripts/test_block.js
@@ -0,0 +1,16 @@
+Blockly.Blocks['hello_world'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Hello world, My name is ")
+        .appendField(new Blockly.FieldTextInput("Joe"), "NAME");
+    this.setColour(180);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['hello_world'] = function(block) {
+  var text_name = block.getFieldValue('NAME');
+  // TODO: Assemble Python into code variable.
+  var code = 'print("Hello World, My name is '+ text_name+ '")';
+  return code;
+};
