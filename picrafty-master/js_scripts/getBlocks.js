0f914fa12abcb01d9ce790e92376c670922bb64d
Richie Burch <rburch@gmail.com>
2019-06-12 15:42:19 -0500

    first draft of getBlocks block

--- picrafty-master/js_scripts/getBlocks.js
+++ picrafty-master/js_scripts/getBlocks.js
@@ -1,17 +1,39 @@
-Blockly.Blocks['getBlocks'] = {
-    init: function() {
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.appendDummyInput()
-            .appendField("Get Blocks");
-
-        this.setColour(230);
-        this.setTooltip("Get multiple blocks in a cuboid.");
-        this.setHelpUrl("");
-    }
+Blockly.Blocks['getblocks'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Get list of block types.");
+    this.appendDummyInput()
+        .appendField("x0:")
+        .appendField(new Blockly.FieldTextInput("0"), "X0COORD")
+        .appendField("x1:")
+        .appendField(new Blockly.FieldTextInput("0"), "X1COORD");
+    this.appendDummyInput()
+        .appendField("y0:")
+        .appendField(new Blockly.FieldTextInput("0"), "Y0COORD")
+        .appendField("y1:")
+        .appendField(new Blockly.FieldTextInput("0"), "Y1COORD");
+    this.appendDummyInput()
+        .appendField("z0:")
+        .appendField(new Blockly.FieldTextInput("0"), "Z0COORD")
+        .appendField("z1:")
+        .appendField(new Blockly.FieldTextInput("0"), "Z1COORD");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("Gets types of blocks in a cuboid. Returns a list of ints.");
+ this.setHelpUrl("");
+  }
 };
-Blockly.Python['getBlocks'] = function(block) {
+Blockly.Python['getblocks'] = function(block) {
+  var x0 = block.getFieldValue('X0COORD');
+  var x1 = block.getFieldValue('X1COORD');
+  var y0 = block.getFieldValue('Y0COORD');
+  var y1 = block.getFieldValue('Y1COORD');
+  var z0 = block.getFieldValue('Z0COORD');
+  var z1 = block.getFieldValue('Z1COORD');
 
-    var code = '';
-    return code;
+  let argumentString = x0 + ',' + y0 + ',' + z0 + ',' + x1 + ',' + y1 + ',' + z1;
+
+  var code = 'mc.getBlocks(' + argumentString + ')';
+
+  return [code, Blockly.Python.ORDER_NONE];
 };
