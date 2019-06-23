f4243bf63a0fbc5f3cea3b243fb0bd9e67c18a0a
asunathanr <nrobert2@my.athens.edu>
2019-06-23 14:17:05 -0500

    Added appropriate behavior for all entity blocks

--- picrafty-master/minecraft_blocks/entity.js
+++ picrafty-master/minecraft_blocks/entity.js
@@ -1,23 +1,24 @@
 // FILE: entity.js
-// AUTHOR: Matt Hardin
+// AUTHOR: Matt Hardin, Nathan Robertson
 // PURPOSE: Implement blocks to map to Minecraft().entity methods.
+
+const ENTITY_ID = 'ENTITY_ID';
 
 Blockly.Blocks['entity_getPos'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("get players position");
         this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
         this.setColour(230);
         this.setTooltip("Sets player position to top of given x,y,z coordinates");
         this.setOutput(true, null);
-        this.setColour(230);
         this.setTooltip("Returns player direction as a Vec3 object.");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_getPos'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
+    var code = 'mc.player.getPos(' + block.getFieldValue(ENTITY_ID) +')\n';
     return [code, Blockly.Python.ORDER_NONE];
 };
 
@@ -25,39 +26,48 @@ Blockly.Python['entity_getPos'] = function (block) {
 Blockly.Blocks['entity_setPos'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
+            .appendField("Set entity's position");
         this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'X');
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'Y');
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'Z');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
         this.setColour(230);
-        this.setTooltip("Sets player position to top of given x,y,z coordinates");
-        this.setOutput(true, null);
-        this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
+        this.setTooltip("Sets entity's position to top of given x,y,z coordinates");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_setPos'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
-    return [code, Blockly.Python.ORDER_NONE];
+    let entityId = block.getFieldValue(ENTITY_ID);
+    let x = block.getFieldValue('X');
+    let y = block.getFieldValue('Y');
+    let z = block.getFieldValue('Z');
+    let arguments = [entityId, x, y, z].join(',');
+    let code = 'mc.entity.setPos('+ arguments + ')' +'\n';
+    return code;
 };
 
 
-Blockly.Blocks['enetity_getTilePos'] = {
+Blockly.Blocks['entity_getTilePos'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
+            .appendField("Get Tile Position");
         this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
         this.setColour(230);
-        this.setTooltip("Sets player position to top of given x,y,z coordinates");
+        this.setTooltip("Gets entity tile position to top of given x,y,z coordinates");
         this.setOutput(true, null);
-        this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_getTilePos'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
+    let entityId = block.getFieldValue(ENTITY_ID);
+    let code = 'mc.entity.getTilePos(' + entityId + ')\n';
     return [code, Blockly.Python.ORDER_NONE];
 };
 
@@ -65,39 +75,46 @@ Blockly.Python['entity_getTilePos'] = function (block) {
 Blockly.Blocks['entity_setTilePos'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
+            .appendField("Set entity tile position");
         this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'X');
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'Y');
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldNumber(), 'Z');
         this.setColour(230);
         this.setTooltip("Sets player position to top of given x,y,z coordinates");
-        this.setOutput(true, null);
-        this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_setTilePos'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
-    return [code, Blockly.Python.ORDER_NONE];
+    let entityId = block.getField(ENTITY_ID);
+    let X = block.getFieldValue('X');
+    let Y = block.getFieldValue('Y');
+    let Z = block.getFieldValue('Z');
+    var code = 'mc.player.setTilePos(' + [entityId, X, Y, Z].join(',') +  ')\n';
+    return code;
 };
 
 
 Blockly.Blocks['entity_getRotation'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
+            .appendField("Get Entity Rotation.");
         this.appendDummyInput()
             .appendField(new Blockly.FieldNumber(), 'Entity_ID');
-        this.setColour(230);
-        this.setTooltip("Sets player position to top of given x,y,z coordinates");
         this.setOutput(true, null);
         this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
+        this.setTooltip("Get entity rotation");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_getRotation'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
+    let code = 'mc.entity.getRotation(' + block.getFieldValue(ENTITY_ID) + ')\n';
     return [code, Blockly.Python.ORDER_NONE];
 };
 
@@ -105,19 +122,18 @@ Blockly.Python['entity_getRotation'] = function (block) {
 Blockly.Blocks['entity_getPitch'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
-        this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField("Get Player Pitch");
+        this.appendDummyInput("Entity ID")
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
         this.setColour(230);
-        this.setTooltip("Sets player position to top of given x,y,z coordinates");
         this.setOutput(true, null);
-        this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
+        this.setTooltip("Returns entity pitch");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_getPitch'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
+    let entityId = block.getFieldValue(ENTITY_ID);
+    let code = 'mc.entity.getPitch(' + entityId + '\n';
     return [code, Blockly.Python.ORDER_NONE];
 };
 
@@ -125,18 +141,17 @@ Blockly.Python['entity_getPitch'] = function (block) {
 Blockly.Blocks['entity_getDirection'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Get Player Direction");
-        this.appendDummyInput()
-            .appendField(new Blockly.FieldNumber(), 'Entity_ID');
+            .appendField("Get Entity Direction");
+        this.appendDummyInput("Entity ID")
+            .appendField(new Blockly.FieldNumber(), ENTITY_ID);
         this.setColour(230);
         this.setTooltip("Sets player position to top of given x,y,z coordinates");
         this.setOutput(true, null);
-        this.setColour(230);
-        this.setTooltip("Returns player direction as a Vec3 object.");
+        this.setTooltip("Returns entity direction as a Vec3 object.");
         this.setHelpUrl("");
     }
 };
 Blockly.Python['entity_getDirection'] = function (block) {
-    var code = 'mc.player.getDirection()\n';
+    let code = 'mc.entity.getDirection(' + block.getFieldValue(ENTITY_ID) + ')\n';
     return [code, Blockly.Python.ORDER_NONE];
 };
