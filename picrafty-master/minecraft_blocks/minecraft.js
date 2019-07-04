92289253404eef6c56b380448611447a115b0faa
Richie Burch <rburch@gmail.com>
2019-07-04 15:39:58 -0500

    changed design of setBlock block

--- picrafty-master/minecraft_blocks/minecraft.js
+++ picrafty-master/minecraft_blocks/minecraft.js
@@ -294,97 +294,30 @@ Blockly.Blocks['setBlocks'] = {
   };
 
 
-Blockly.Blocks['setBlock'] = {
-    init: function () {
-        // custom dropdown object that will run update shape helper function when option is changed
-        var dropdown = new Blockly.FieldDropdown([["standard", "STANDARD"], ["vec3", "VEC3"], ["block", "BLOCK"]], function (vec_input) {
-            this.sourceBlock_.updateShape_(vec_input);
-        });
-
-        // initial state of block
-        this.appendDummyInput()
-            .appendField("Set a single block.")
-            .appendField(dropdown, "CONFIG");
-        this.appendDummyInput("XES")
-            .appendField("x:")
-            .appendField(new Blockly.FieldTextInput("0"), "X");
-        this.appendDummyInput("YES")
-            .appendField("y:")
-            .appendField(new Blockly.FieldTextInput("0"), "Y")
-            .appendField("Block Type:")
-            .appendField(new Blockly.FieldTextInput("1"), "TYPE");
-        this.appendDummyInput("ZES")
-            .appendField("z:")
-            .appendField(new Blockly.FieldTextInput("0"), "Z")
-            .appendField("Block      Id:")
-            .appendField(new Blockly.FieldTextInput("0"), "ID");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    },
-
-     // function that will allow changes to save to xml
-     mutationToDom: function () {
-        var containter1 = document.createElement('mutation');
-        var vecInput = (this.getFieldValue("CONFIG") === 'VEC3');
-        containter1.setAttribute('vec_input', vecInput);
-        return containter1;
-    },
-
-    // function to load block from xml?
-    domToMutation: function (xmlElement) {
-        var hasVecInput = (xmlElement.getAttribute('vec_input' === 'true'));
-        console.log("hasVecInput = ", hasVecInput);
-        this.updateShape_(hasVecInput);
-    },
-
-    // function to update shape of block on dropdown change
-    updateShape_: function (vecInput) {
-
-
-        if (vecInput) {  // make sure an option has been passed
-            if (vecInput === "VEC3") {  // if the vec3 option has been selected
-                addVectorInput(this);
-                addBlockStandard(this);
-
-            } else if (vecInput === "STANDARD") { // if the standard option has been selected
-                addVectorCoordinatesSingle(this);
-                addBlockStandard(this);
-                
-            } else if (vecInput === "BLOCK") { // if the block option has been selected
-               addBlockInput(this);
-               addVectorCoordinatesSingle(this);
-            }
-        }
+  Blockly.Blocks['setBlock'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("Set a single block.");
+      this.appendValueInput("vec")
+          .setCheck(null)
+          .appendField("Vector:");
+      this.appendValueInput("block")
+          .setCheck(null)
+          .appendField("Block:");
+      this.setOutput(true, null);
+      this.setColour(230);
+   this.setTooltip("Set a single block.");
+   this.setHelpUrl("");
     }
-};
-Blockly.Python['setBlock'] = function (block) {
-
-    var status = block.getFieldValue('CONFIG');
-
-    if(status === "STANDARD") {
-        var X = block.getFieldValue('X');
-        var Y = block.getFieldValue('Y');
-        var Z = block.getFieldValue('Z');
-        var blockId = block.getFieldValue('blockId');
-        var blockType = block.getFieldValue('blockType');
-        let argumentString = X + ',' + Y + ',' + Z + ',' + blockId + ',' + blockType;
-        var code = 'mc.setBlock(' + argumentString + ')\n';
-        return code;
-    } else if (status === "VEC3") {
-        var varName = Blockly.Python.valueToCode(block, "VEC", Blockly.Python.ORDER_ATOMIC);
-        var blockType = block.getFieldValue("TYPE");
-        var blockId = block.getFieldValue("ID");
-        console.log(varName);
-        let argumentString = varName + ', ' + blockType + ', ' + blockId;
-        var code = 'mc.setBlock(' + argumentString + ')\n';
-        return code;
-    }
-
-   
-};
+  };
+  Blockly.Python['setBlock'] = function(block) {
+    var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
+    var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
+    // TODO: Assemble Python into code variable.
+    var code = 'mc.setBlock('+value_vec+', '+value_block+')\n';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+  };
 
 
 Blockly.Blocks['saveCheckpoint'] = {
