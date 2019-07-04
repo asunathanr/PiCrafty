23d6ac901bfe304d689ec83d1470d0120dc9188a
asunathanr <nrobert2@my.athens.edu>
2019-07-04 17:24:28 -0500

    Changed player.setTilePos to json

--- picrafty-master/minecraft_blocks/player.js
+++ picrafty-master/minecraft_blocks/player.js
@@ -8,13 +8,6 @@
 
 const BLOCK_COLOR = 230;
 
-const X_NAME = "XES";
-const Y_NAME = "YES";
-const Z_NAME = "ZES";
-
-const X_FIELD = "X";
-const Y_FIELD = "Y";
-const Z_FIELD = "Z";
 
 Blockly.defineBlocksWithJsonArray([
 
@@ -33,6 +26,24 @@ Blockly.defineBlocksWithJsonArray([
         "nextStatement": null,
         "colour": BLOCK_COLOR,
         "tooltip": "Sets player position to new x,y,z coordinates",
+        "helpUrl": "",
+    },
+
+    // BLOCK: mc.player.setTilePos()
+    {
+        "type":"setTilePos",
+        "message0": "Set Player tile position",
+        "message1": "%1",
+        "args1": [
+            {
+                "type": "input_value",
+                "name": "VEC3",
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": BLOCK_COLOR,
+        "tooltip": "Sets position underneath player to new x,y,z coordinates",
         "helpUrl": "",
     },
 
@@ -108,65 +119,6 @@ Blockly.Blocks['player_setting'] = {
 };
 
 
-Blockly.Blocks['setTilePos'] = {
-    init: function () {
-        const INPUT_TYPES = [["standard", "STANDARD"], ["vec3", "VEC3"]];
-        let dropdown = new Blockly.FieldDropdown(INPUT_TYPES, function (vec_input) {
-            this.sourceBlock_.updateShape_(vec_input);
-        });
-
-        // initial state of block
-        this.appendDummyInput()
-            .appendField("Set Player Tile Position")
-            .appendField(dropdown, "CONFIG");
-
-        this.appendDummyInput(X_NAME)
-            .appendField('x:')
-            .appendField(new Blockly.FieldNumber(), X_FIELD);
-        this.appendDummyInput(Y_NAME)
-            .appendField('y:')
-            .appendField(new Blockly.FieldNumber(), Y_FIELD);
-        this.appendDummyInput(Z_NAME)
-            .appendField('z:')
-            .appendField(new Blockly.FieldNumber(), Z_FIELD);
-
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(BLOCK_COLOR);
-        this.setTooltip("Sets player position to top of given x,y,z coordinates");
-        this.setHelpUrl("");
-    },
-
-    mutationToDom: function () {
-        let blockCache = document.createElement('mutation');
-        let vecInput = (this.getFieldValue("CONFIG") === 'VEC3');
-        blockCache.setAttribute('vec_input', vecInput);
-        return blockCache;
-    },
-
-    // function to load block from xml?
-    domToMutation: function (xmlElement) {
-        let hasVecInput = (xmlElement.getAttribute('vec_input' === 'true'));
-        console.log("hasVecInput = ", hasVecInput);
-        this.updateShape_(hasVecInput);
-    },
-
-    // function to update shape of block on dropdown change
-    updateShape_: function (vecInput) {
-        if (vecInput) {  // make sure an option has been passed
-            if (vecInput === "VEC3") {  // if the vec3 option has been selected
-                addVectorInput(this);
-                addBlockInput(this);
-
-            } else if (vecInput === "STANDARD") { // if the standard option has been selected
-                addVectorCoordinatesSingle(this);
-                addBlockInput(this);
-            }
-        }
-    },
-};
-
-
 Blockly.Python['getDirection'] = function (block) {
     let code = 'mc.player.getDirection()\n';
     return [code, Blockly.Python.ORDER_NONE];
@@ -204,12 +156,8 @@ Blockly.Python['setPos'] = function (block) {
 
 
 Blockly.Python['setTilePos'] = function (block) {
-    let arguments = [
-        block.getFieldValue('X'),
-        block.getFieldValue('Y'),
-        block.getFieldValue('Z')
-    ].join(", ");
-    return 'mc.player.setTilePos(' + arguments + ')\n';
+    let arguments = Blockly.Python.valueToCode(block,"VEC3", Blockly.Python.ORDER_ATOMIC);
+    return 'mc.player.setPos(' + arguments + ')\n';
 };
 
 
@@ -219,75 +167,3 @@ Blockly.Python['player_setting'] = function (block) {
     let arguments = block.getFieldValue("SETTING") + ',' + status;
     return 'mc.player.setting(' + arguments + ')\n';
 };
-
-
-// functions that are reused to shape google_blocks in multiple mutators
-
-// replace manual vector coordinates with vector input
-function addVectorInput(block) {
-    if (block.getInput("XES")) {
-        block.removeInput("XES");
-    }
-    if (block.getInput("YES")) {
-        block.removeInput("YES");
-    }
-    if (block.getInput("ZES")) {
-        block.removeInput("ZES");
-    }
-    if (!block.getInput("VEC")) {
-        block.appendValueInput("VEC");
-    }
-
-    var xCheck = new Blockly.FieldCheckbox("FALSE", function(xChecked) {
-        block.sourceBlock_.updateShape_(xChecked);
-    });
-}
-
-
-// replace vector input with manual vector coordinates
-function addVectorCoordinatesSingle(block) {
-    if (block.getInput("VEC")) {
-        block.removeInput("VEC");
-    }
-    if (!block.getInput("XES")) {
-        block.appendDummyInput("XES")
-            .appendField("x:")
-            .appendField(new Blockly.FieldTextInput("0"), "X");
-    }
-
-    if (!block.getInput("YES")) {
-        block.appendDummyInput("YES")
-            .appendField("y:")
-            .appendField(new Blockly.FieldTextInput("0"), "Y");
-    }
-
-    if (!block.getInput("ZES")) {
-        block.appendDummyInput("ZES")
-            .appendField("z:")
-            .appendField(new Blockly.FieldTextInput("0"), "Z");
-    }
-}
-
-// replace input block entry with manual fields
-function addBlockStandard(block) {
-    if(block.getInput("BLOCKINPUT")) {
-        block.removeInput("BLOCKINPUT");
-    }
-    if(!block.getInput("BLOCKSTANDARD")) {
-        block.appendDummyInput("BLOCKSTANDARD")
-            .appendField("Block Type:")
-            .appendField(new Blockly.FieldTextInput("1"), "TYPE")
-            .appendField("Block ID:")
-            .appendField(new Blockly.FieldTextInput("0"), "ID");
-    }
-}
-
-// replace manual block entries with input type
-function addBlockInput(block) {
-    if(!block.getInput("BLOCKINPUT")) {
-        block.appendValueInput("BLOCKINPUT");
-    }
-    if(block.getInput("BLOCKSTANDARD")) {
-        block.removeInput("BLOCKSTANDARD");
-    }
-}
