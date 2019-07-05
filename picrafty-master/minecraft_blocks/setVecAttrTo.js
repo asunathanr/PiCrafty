6218d83e58c6fd9a5f28283a3a6941b8ce95a24f
asunathanr <nrobert2@my.athens.edu>
2019-07-04 23:23:49 -0500

    Defined vector blocks with json

--- picrafty-master/minecraft_blocks/setVecAttrTo.js
+++ picrafty-master/minecraft_blocks/setVecAttrTo.js
@@ -1,3 +1,28 @@
+Blockly.defineBlocksWithJsonArray([
+
+    // BLOCK: set_vector_attribute_to
+    {
+        "type": "set_vector_attribute_to",
+        "message0": "Set %1 to %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "LEFT"
+            },
+            {
+                "type": "input_value",
+                "name": "RIGHT"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": BLOCK_COLOR,
+        "tooltip": "Set variable attribute to a value.",
+        "helpUrl": ""
+    }
+]);
+
+/**
 Blockly.Blocks['set_vector_attribute_to'] = {
     init: function() {
       this.appendValueInput("LEFT")
@@ -14,7 +39,7 @@ Blockly.Blocks['set_vector_attribute_to'] = {
    this.setHelpUrl("");
     }
   };
-
+*/
   Blockly.Python['set_vector_attribute_to'] = function(block) {
     var value_left = Blockly.Python.valueToCode(block, 'LEFT', Blockly.Python.ORDER_ATOMIC);
     var value_right = Blockly.Python.valueToCode(block, 'RIGHT', Blockly.Python.ORDER_ATOMIC);
